847c711ea2f73fb6f02148b9d5f22482ae8276da
Ngesa Marvin <ngesa.marvin@gmail.com>
2024-10-17 20:05:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -115,3 +115,5 @@ def simulate_workload(seconds: int = 1):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+#This is the old code
